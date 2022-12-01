435a7f5398cc7db3ecbe62aa4f974fb8bad0efbd
Carsten Jørgensen <carstenj@gmail.com>
2022-12-01 14:33:34 +0100

    rate limiting feature

--- Program.cs
+++ Program.cs
@@ -1,9 +1,28 @@
+using Microsoft.AspNetCore.RateLimiting;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddRateLimiter(limiterOptions =>
+{
+    limiterOptions.OnRejected = (context, rateLimit) =>
+    {
+        context.HttpContext.Response.StatusCode = StatusCodes.Status429TooManyRequests;
+        return new ValueTask();
+    };
+    limiterOptions.AddFixedWindowLimiter(policyName: "fixed", options =>
+    {
+        options.PermitLimit = 3;
+        options.Window = TimeSpan.FromSeconds(10);
+        options.QueueLimit = 1;
+    });
+});
+
 var app = builder.Build();
+
+app.UseRateLimiter();
 
 if (app.Environment.IsDevelopment())
 {
@@ -17,13 +36,13 @@ var summaries = new[]
 };
 
 app.MapGet("/weatherforecast", () =>
-    Results.Ok(Enumerable.Range(1, 10).Select(index =>
+    TypedResults.Ok(Enumerable.Range(1, 10).Select(index =>
         new WeatherForecast
         (
             DateTime.Now.AddDays(index),
             Random.Shared.Next(-15, 40),
             summaries[Random.Shared.Next(summaries.Length)]
-        )))).Produces<WeatherForecast[]>();
+        )))).RequireRateLimiting("fixed");
 
 app.Run();
 
